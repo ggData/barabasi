0b70046f550f216b43085709ab5f62b13ca138de
ggData <gauden.data@gmail.com>
2014-06-14 16:11:47 +0200

    Add README

--- sna/server.R
+++ sna/server.R
@@ -3,34 +3,44 @@ library(igraph)
 library(ggplot2)
 
 shinyServer(function(input, output) {
-    
+  
     graph <- reactive({ 
         if (input$graph == "barabasi") {
-            barabasi.game(input$n)
-        } else if (input$graph == "erdos") {
-            erdos.renyi.game(input$n, 1/input$n)
+            barabasi.game(input$n, directed=FALSE)
         } else {
-            watts.strogatz.game(1, input$n, 3, 1/input$n)
-        } 
+            erdos.renyi.game(input$n, input$p)
+        }
     })
     
-    output$plot <- renderPlot({
+    output$netplot <- renderPlot({
         plot(graph())
     })
     
     # Generate a summary of the data
     output$path <- renderPrint({
-        average.path.length(graph())
+      average.path.length(graph())
     })
-     
+    
+    output$cluster_number <- renderPrint({
+      c <- clusters(graph())
+      c$no
+    })
+    
+    output$graph_density <- renderPrint({
+      graph.density(graph())
+    })
+    
     output$degdist <- renderPlot({
-        p <- degree.distribution(graph())
-        d <- 1:length(p)
-        data <- data.frame(proportion=p, degree=d)
-        p <- ggplot(data, aes(x=degree, y=proportion))
-        p <- p + geom_point() + geom_line() + ylim(0,1.0)
-        p <- p + scale_x_continuous(breaks=c(1:max(d)))
-        print(p)
+      p <- degree.distribution(graph())
+      d <- 1:length(p)
+      data <- data.frame(proportion=p, degree=d)
+      p <- ggplot(data, aes(x=degree, y=proportion))
+      p <- p + geom_point() + geom_line() + ylim(0,1.0)
+      p <- p + xlab("Degree (Number of neighbours)")
+      p <- p + ylab("Proportion of all Nodes")
+      p <- p + scale_x_continuous(breaks=c(1:max(d)))
+      print(p)
     })
     
 })
+
--- sna/ui.R
+++ sna/ui.R
@@ -11,25 +11,53 @@ shinyUI(fluidPage(
         sidebarPanel(
             radioButtons("graph", "Graph type:",
                          c("Preferential Attachment" = "barabasi",
-                           "Random Attachment" = "erdos",
-                           "Small World" = "watts")),
+                           "Random Attachment" = "erdos")
+                         ),
             br(),
-            
             sliderInput("n", 
                         "Number of nodes:", 
                         value = 25,
                         min = 5, 
-                        max = 50)
+                        max = 100),
+            br(),
+            conditionalPanel("input.graph == 'erdos'",
+                             sliderInput("p", 
+                                         "Probability of attachment:", 
+                                         value = 0.1,
+                                         min = 0, 
+                                         max = 0.3)
+                             )
         ),
         
         # Show a tabset that includes a plot, summary, and table view
         # of the generated distribution
-        mainPanel(
-            tabsetPanel(type = "tabs", 
-                        tabPanel("Plot", plotOutput("plot")),
-                        tabPanel("Path Length", verbatimTextOutput("path")),
-                        tabPanel("Degree", plotOutput("degdist"))
-            )
-        )
+        mainPanel(tabsetPanel(type = "tabs", 
+                              tabPanel("Network", 
+                                       verticalLayout(
+                                         plotOutput("netplot"),
+                                         h4("Average path length"),
+                                         p("The average number of hops from any node to any other 
+                                           node in the graph."),
+                                         verbatimTextOutput("path"),
+                                         h4("Number of clusters"),
+                                         p("The number of groups of connected nodes in the graph. 
+                                           (A solitary node counts as a cluster)."),
+                                         verbatimTextOutput("cluster_number"),
+                                         h4("Graph Density"),
+                                         p("Ratio of actual number of edges to total 
+                                           number of possible edges"),
+                                         verbatimTextOutput("graph_density")
+                                       )),
+                              tabPanel("Degree Distribution", 
+                                       verticalLayout(
+                                         p("Relative distribution: the proportion of nodes having a given 
+                                           number of connected neighbours."),
+                                         hr(),
+                                         plotOutput("degdist")
+                                       )
+                              )
+                              )
+                  )
     )
 ))
+
